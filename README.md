0e30776a950dc06051d36b85579017977b82256c
Allie <alliesafronova@gmail.com>
2024-11-01 18:35:23 +0700

    feat: add postgres integration

--- README.md
+++ README.md
@@ -7,3 +7,12 @@
 
 ## Run
 `docker run --network host stub-app`
+
+## Test
+### Find user
+`curl http://localhost/8081/api/getUser/{login}`
+
+### Add user
+`curl -X POST -H "Content-Type: aplication/json" -d '{ "login": "new-user", "password": "new-password", "email": "new-user@example.com" }' http://localhost/8081/api/postUser -v`
+
+`curl -X POST -H "Content-Type: aplication/json" -d '{ "login": "new-user", "password": "new-password", "email": "new-user@example.com" }' http://localhost/8081/api/postUser2 -v`
--- src/main/java/com/application/stub/controller/MainController.java
+++ src/main/java/com/application/stub/controller/MainController.java
@@ -1,18 +1,25 @@
 package com.application.stub.controller;
+import com.application.stub.entity.DBService;
 import com.application.stub.entity.User;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
 public class MainController {
-    private static final User STATIC_USER = new User("Alisa", "XXX0088", "email");
+    @Autowired
+    private DBService dbService;
     private void addRandomDelay() {
         try {
             int delay = (int) (1000 + Math.random() * 1000);
@@ -21,37 +28,47 @@ public class MainController {
             Thread.currentThread().interrupt();
         }
     }
-    @GetMapping("/getUser")
-    public ResponseEntity<?> getUser() {
+    @GetMapping("/getUser/{login}")
+    public ResponseEntity<?> getUserbyLogin(@PathVariable String login) {
         addRandomDelay();
-        return ResponseEntity.ok(STATIC_USER);
+        try {
+            User user = dbService.getUserByLogin(login);
+            return ResponseEntity.ok(user);
+        } catch (SQLException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"error\": \"Database error\"}");
+        }
 
     }
 
     @PostMapping("/postUser")
     public ResponseEntity<?> postUser(@RequestBody @Valid User user) {
         addRandomDelay();
-        return ResponseEntity.ok(user);
+        try {
+            dbService.addUser(user);
+            return ResponseEntity.status(HttpStatus.CREATED).body("User created successfully");
+        } catch (SQLException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"error\": \"Database error\"}");
+        }
     }
+
     @PostMapping("/postUser2")
     public ResponseEntity<?> postUser2(@RequestBody Map<String, Object> userMap) {
-        if (!userMap.containsKey("login") || !userMap.containsKey("password")  || !userMap.containsKey("email")) {
-            return ResponseEntity.badRequest().body("{\"error\": \"Login, password and email are required\"}");
-        }
-        if (userMap.size() > 3) {
-            return ResponseEntity.badRequest().body("{\"error\": \"Extra field detected\"}");
+        if (!userMap.containsKey("login") || !userMap.containsKey("password")  || !userMap.containsKey("email") || userMap.size() > 3) {
+            return ResponseEntity.badRequest().body("{\"error\": \"Bad json\"}");
         }
         String login = (String) userMap.get("login");
         String password = (String) userMap.get("password");
         String email = (String) userMap.get("email");
-        if (login == null || password == null || email == null) {
-            return ResponseEntity.badRequest().body("{\"error\": \"User fields cannot be null\"}");
-        }
-        if (login.isEmpty() || password.isEmpty() || email.isEmpty()) {
-            return ResponseEntity.badRequest().body("{\"error\": \"User fields cannot be empty\"}");
+        if (login == null || password == null || email == null || login.isEmpty() || password.isEmpty() || email.isEmpty()) {
+            return ResponseEntity.badRequest().body("{\"error\": \"Bad json\"}");
         }
         addRandomDelay();
         User user = new User(login, password, email);
-        return ResponseEntity.ok(user);
+        try {
+            dbService.addUser(user);
+            return ResponseEntity.status(HttpStatus.CREATED).body("User created successfully");
+        } catch (SQLException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"error\": \"Database error\"}");
+        }
     }
 }
